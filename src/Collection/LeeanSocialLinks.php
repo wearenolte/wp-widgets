b86a57f746e6120a01d31a86c3d4abc2948f84ba
Caridad Zapatero Contreras <zzuga@zzuga.com>
2016-04-05 20:49:42 +0200

    Social Links element

---
+++ src/Collection/LeeanSocialLinks.php
@@ -0,0 +1,32 @@
+<?php namespace Leean\Widgets\Collection;
+
+use Leean\Widgets\Models\AbstractWidget;
+use Leean\Acf;
+
+/**
+ * Class LeeanSocialLinks.
+ *
+ * @package Leean\Widgets\Collection
+ */
+class LeeanSocialLinks extends AbstractWidget
+{
+	/**
+	 * Constructor.
+	 */
+	public function __construct() {
+		parent::__construct( 'Leean Social Links', 'Display list of social links' );
+	}
+
+	/**
+	 * Get the widget's data.
+	 *
+	 * @return mixed
+	 */
+	public function get_data() {
+		$data = parent::get_data();
+
+		$data['items'] = Acf::get_option_field( 'items' );
+
+		return $data;
+	}
+}
